2b89f0ba5232bcca11bf12048d4b1bf03beb922c
Rushiraj11 <grushiraj95@gmail.com>
2021-09-20 22:28:18 +0530

    Added: Password Rule4 validation using JunitTest

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -89,5 +89,16 @@ public class UserRegistrationTest {
         boolean result = userRegistration.passwordRule3("hkishK");
         Assert.assertEquals(false, result);
     }
+    @Test
+    public void givenPasswordRule4_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.passwordRule4("Mikidhie@22");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPasswordRule4_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.passwordRule4("mikdhos");
+        Assert.assertEquals(false, result);
+    }
 
 }
