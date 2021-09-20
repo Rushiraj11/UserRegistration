4b69b589f5fd1f1490778810f5ea50aed4b66288
Rushiraj11 <grushiraj95@gmail.com>
2021-09-20 20:03:16 +0530

    Added: Password Rule1 validation using JunitTest

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -18,6 +18,7 @@ public class UserRegistrationTest {
         boolean result = userRegistration.firstName("rushiraj");
         Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenLastName_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.lastName("Ghatge");
@@ -30,6 +31,7 @@ public class UserRegistrationTest {
         Assert.assertEquals(false, result);
 
     }
+
     @Test
     public void givenEmail_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.emailAddress("abc@gmail.com");
@@ -41,6 +43,7 @@ public class UserRegistrationTest {
         boolean result = userRegistration.emailAddress("abc#/@gmail.com");
         Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenPhoneNumber_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.phoneNumber("91 9809874523");
@@ -50,6 +53,18 @@ public class UserRegistrationTest {
     @Test
     public void givenPhoneNumber_WhenOurOfFormat_ShouldReturnFlase() {
         boolean result = userRegistration.phoneNumber("+91 99660315270");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.passwordRule1("aklmojmu");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.passwordRule1("abARd");
         Assert.assertEquals(false, result);
     }
 }
