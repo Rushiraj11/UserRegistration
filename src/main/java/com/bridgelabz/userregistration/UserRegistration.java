2673ab84d1412773778789a5bcc4e3440c0596d2
Rushiraj11 <grushiraj95@gmail.com>
2021-09-26 17:35:21 +0530

    Added:EmailId validation using Parameterized Test method

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -20,7 +20,7 @@ public class UserRegistration {
         return matcher.matches();
     }
     public boolean emailAddress(String email) {
-        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        String regex = "^[a-z]{3,}([_+-.]?[a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+([.,][a-z]{2,3}+)*$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
