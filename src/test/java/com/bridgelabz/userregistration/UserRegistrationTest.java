242f37c0bec7ef198feb6c14663260eb38a5bfeb
Rushiraj11 <grushiraj95@gmail.com>
2021-09-26 16:24:46 +0530

    Added:Vaidation of EmailId's

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -67,6 +67,7 @@ public class UserRegistrationTest {
         boolean result = userRegistration.passwordRule1("abARd");
         Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenPasswordRule2_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.passwordRule2("Abcdefghij");
@@ -78,6 +79,7 @@ public class UserRegistrationTest {
         boolean result = userRegistration.passwordRule2("abcdefghij");
         Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenPasswordRule3_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.passwordRule3("RUfgioa22");
@@ -89,6 +91,7 @@ public class UserRegistrationTest {
         boolean result = userRegistration.passwordRule3("hkishK");
         Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenPasswordRule4_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.passwordRule4("Mikidhie@22");
@@ -101,4 +104,16 @@ public class UserRegistrationTest {
         Assert.assertEquals(false, result);
     }
 
+    @Test
+    public void givenEmail1_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.emailIdValidator("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail1_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.emailIdValidator("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
+
+    }
 }
